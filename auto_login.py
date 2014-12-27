36437baddfae0cedf011f37b4227d3752203420e
Kestutis Baltronas <kestutisbalt@gmail.com>
2014-12-27 18:19:49 +0200

    Auto login implementation.

--- auto_login.py
+++ auto_login.py
@@ -1,10 +1,23 @@
 import sys
+import mechanize
+
+
+# Logins to filmai.in with user credentials.
+def auto_login(user, password):
+	browser = mechanize.Browser()
+	browser.open('http://filmai.in')
+	browser.select_form(nr = 0)
+	browser['login_name'] = user
+	browser['login_password'] = password
+	browser.submit('image')
 
 
 def main(args = None):
-	if len(args) < 2:
-		print("Configuration file is not specified.")
+	if len(args) < 3:
+		print("Bad parameters.")
 		exit(-1)
+
+	auto_login(args[1], args[2])
 
 
 if __name__ == "__main__":
